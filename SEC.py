fa4d78fb1db30c271bee05d519fd5158061740e0
wahyu-19 <wahyupujiati11632@gmail.com>
2025-09-11 01:19:35 +0700

    Update SEC.py

--- SEC.py
+++ SEC.py
@@ -21,7 +21,7 @@ def proses_peramalan(file):
     df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], errors='coerce')
     df = df.dropna(subset=['TANGGAL']).sort_values("TANGGAL")
 
-    df_nan = df.dropna(subset=['RR'])
+        df_nan = df.dropna(subset=['RR'])
     scaler = MinMaxScaler(feature_range=(0, 1))
     df_nan['RR_norm'] = scaler.fit_transform(df_nan[['RR']])
 
@@ -147,3 +147,4 @@ if uploaded_file is not None:
 
     csv = df_forecast.to_csv(index=False).encode("utf-8")
     st.download_button("💾 Download Hasil Peramalan", csv, "hasil_peramalan.csv", "text/csv")
+
